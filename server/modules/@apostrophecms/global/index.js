1549ff7bcf7690e1d35e011ffd673b73512c65ec
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-04-23 15:18:15 +0200

    Footer

--- server/modules/@apostrophecms/global/index.js
+++ server/modules/@apostrophecms/global/index.js
@@ -1,8 +1,37 @@
 module.exports = {
   fields: {
     add: {
-      primaryNav: {
-        label: 'Primary site navigation',
+      headerNav: {
+        label: 'Header navigation links',
+        type: 'array',
+        titleField: 'label',
+        help: 'Add, remove, and reorder navigation items.',
+        inline: true,
+        draggable: true,
+        fields: {
+          add: {
+            label: {
+              label: 'Nav item label',
+              type: 'string'
+            },
+            _page: {
+              label: 'Page to link',
+              type: 'relationship',
+              withType: '@apostrophecms/page',
+              max: 1,
+              required: true,
+              builders: {
+                project: {
+                  title: 1,
+                  _url: 1
+                }
+              },
+            },
+          }
+        }
+      },
+      footerNav: {
+        label: 'Footer navigation links',
         type: 'array',
         titleField: 'label',
         help: 'Add, remove, and reorder navigation items.',
@@ -35,7 +64,7 @@ module.exports = {
       basics: {
         label: 'Basics',
         fields: [
-          'primaryNav',
+          'headerNav', 'footerNav'
         ],
       },
     }
