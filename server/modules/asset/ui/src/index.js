51d91869073ce2468c30c3cfe0f9ceabd6e0cf13
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-05-02 09:23:35 +0200

    Add dotlottie-web player

--- server/modules/asset/ui/src/index.js
+++ server/modules/asset/ui/src/index.js
@@ -1,5 +1,25 @@
+import { DotLottie } from '@lottiefiles/dotlottie-web'
+
 export default () => {
-  apos.util.onReady(() => {
+  apos.util.onReady(async () => {
+    const title = document.querySelector('.pdl-header__title')
+    const lottieFile = await fetch(apos.util.assetUrl('/modules/asset/logo-header-animation.json'))
+    const jsonData = await lottieFile.json()
+
+    const dotLottie = new DotLottie({
+        autoplay: false,
+        loop: true,
+        canvas: document.querySelector('.pdl-header__logo'),
+        data: jsonData
+    })
+
+    title.addEventListener('mouseenter', () => {
+      dotLottie.play()
+    })
+    title.addEventListener('mouseleave', () => {
+      dotLottie.stop()
+    })
+
     const openSideNavButton = document.querySelector('[data-open-side-nav]')
     const closeSideNavButton = document.querySelector('[data-close-side-nav]')
     const sideNav = document.querySelector('[data-side-nav]')
