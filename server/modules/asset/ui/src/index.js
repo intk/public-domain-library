b27701a216672b08f49fd93238cf2ec7eb15c4b3
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-05-02 23:35:23 +0200

    Add footer logo animation

--- server/modules/asset/ui/src/index.js
+++ server/modules/asset/ui/src/index.js
@@ -2,22 +2,36 @@ import { DotLottie } from '@lottiefiles/dotlottie-web'
 
 export default () => {
   apos.util.onReady(async () => {
-    const title = document.querySelector('.pdl-header__title')
-    const lottieFile = await fetch(apos.util.assetUrl('/modules/asset/logo-header-animation.json'))
-    const jsonData = await lottieFile.json()
-
-    const dotLottie = new DotLottie({
-        autoplay: false,
-        loop: true,
-        canvas: document.querySelector('.pdl-header__logo'),
-        data: jsonData
+    const headerTitle = document.querySelector('.pdl-header__title')
+    const headerLottieFile = await fetch(apos.util.assetUrl('/modules/asset/logo-header-animation.json'))
+    const headerJsonData = await headerLottieFile.json()
+    const headerDotLottie = new DotLottie({
+      autoplay: true,
+      loop: true,
+      canvas: document.querySelector('.pdl-header__logo'),
+      data: headerJsonData
+    })
+    headerTitle.addEventListener('mouseenter', () => {
+      headerDotLottie.stop()
+    })
+    headerTitle.addEventListener('mouseleave', () => {
+      headerDotLottie.play()
     })
 
-    title.addEventListener('mouseenter', () => {
-      dotLottie.play()
+    const footerTitle = document.querySelector('.pdl-footer__sentence')
+    const footerLottieFile = await fetch(apos.util.assetUrl('/modules/asset/logo-footer-animation.json'))
+    const footerJsonData = await footerLottieFile.json()
+    const footerDotLottie = new DotLottie({
+      autoplay: true,
+      loop: true,
+      canvas: document.querySelector('.pdl-footer__logo'),
+      data: footerJsonData
     })
-    title.addEventListener('mouseleave', () => {
-      dotLottie.stop()
+    footerTitle.addEventListener('mouseenter', () => {
+      footerDotLottie.stop()
+    })
+    footerTitle.addEventListener('mouseleave', () => {
+      footerDotLottie.play()
     })
 
     const openSideNavButton = document.querySelector('[data-open-side-nav]')
