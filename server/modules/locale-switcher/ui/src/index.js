18cb0d0f73f3a905ca2026cd983389f1565aaefd
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-05-08 22:39:00 +0200

    Mobile menu

--- server/modules/locale-switcher/ui/src/index.js
+++ server/modules/locale-switcher/ui/src/index.js
@@ -1,26 +1,28 @@
 export default () => {
   apos.util.onReady(() => {
-    const section = document.querySelector('.pdl-locale-switcher')
-    const localeSwitcher = document.querySelector('[data-locale-switcher]')
+    const sections = document.querySelectorAll('.pdl-locale-switcher')
 
-    if (localeSwitcher) {
-      localeSwitcher.addEventListener('click', () => {
-        const localeList = document.querySelector('[data-locale-list]')
-        const display = localeList.style.display || 'none'
-        localeList.style.display = display === 'none' ? 'flex' : 'none'
-        localeSwitcher.style.borderColor = display === 'none' ? '#808080' : 'white'
-        localeList.classList.toggle('pdl-locale-switcher__list--animate')
-      })
+    if (sections.length) {
+      for (const section of sections) {
+        const localeSwitcher = section.querySelector('[data-locale-switcher]')
+        localeSwitcher.addEventListener('click', () => {
+          const localeList = section.querySelector('[data-locale-list]')
+          const display = localeList.style.display || 'none'
+          localeList.style.display = display === 'none' ? 'flex' : 'none'
+          localeSwitcher.style.borderColor = display === 'none' ? '#808080' : 'white'
+          localeList.classList.toggle('pdl-locale-switcher__list--animate')
+        })
 
-      section.addEventListener('mouseenter', () => {
-        localeSwitcher.classList.add('pdl-locale-switcher--hover-in')
-        localeSwitcher.classList.remove('pdl-locale-switcher--hover-out')
-      })
+        section.addEventListener('mouseenter', () => {
+          localeSwitcher.classList.add('pdl-locale-switcher--hover-in')
+          localeSwitcher.classList.remove('pdl-locale-switcher--hover-out')
+        })
 
-      section.addEventListener('mouseleave', () => {
-        localeSwitcher.classList.remove('pdl-locale-switcher--hover-in')
-        localeSwitcher.classList.add('pdl-locale-switcher--hover-out')
-      })
+        section.addEventListener('mouseleave', () => {
+          localeSwitcher.classList.remove('pdl-locale-switcher--hover-in')
+          localeSwitcher.classList.add('pdl-locale-switcher--hover-out')
+        })
+      }
     }
   })
 }
