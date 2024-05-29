b42d3ca7c43cae400d33022ad3a4f7149bd154e8
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-05-29 19:29:14 +0200

    Fix hover on title in header + click outside locale list

--- server/modules/locale-switcher/ui/src/index.js
+++ server/modules/locale-switcher/ui/src/index.js
@@ -5,8 +5,9 @@ export default () => {
     if (sections.length) {
       for (const section of sections) {
         const localeSwitcher = section.querySelector('[data-locale-switcher]')
+        const localeList = section.querySelector('[data-locale-list]')
+
         localeSwitcher.addEventListener('click', () => {
-          const localeList = section.querySelector('[data-locale-list]')
           const display = localeList.style.display || 'none'
           localeList.style.display = display === 'none' ? 'flex' : 'none'
           localeSwitcher.style.borderColor = display === 'none' ? '#808080' : 'white'
@@ -21,6 +22,13 @@ export default () => {
         section.addEventListener('mouseleave', () => {
           localeSwitcher.classList.remove('pdl-locale-switcher--hover-in')
           localeSwitcher.classList.add('pdl-locale-switcher--hover-out')
+        })
+        document.addEventListener('click', (event) => {
+          if (!event.target.closest('[data-locale-switcher]') && localeList.classList.contains('pdl-locale-switcher__list--animate')) {
+            localeList.style.display = 'none'
+            localeSwitcher.style.borderColor = '#808080'
+            localeList.classList.remove('pdl-locale-switcher__list--animate')
+          }
         })
       }
     }
