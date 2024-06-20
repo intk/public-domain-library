b01bb4c190bb4faa710eccc92d7610c57b7a8367
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-06-20 10:33:56 +0200

    Social links with hover effect

--- server/modules/@apostrophecms/global/index.js
+++ server/modules/@apostrophecms/global/index.js
@@ -70,12 +70,40 @@ module.exports = {
           },
         },
       },
+      social: {
+        label: 'Social links',
+        type: 'array',
+        titleField: 'url',
+        help: 'Displayed in the footer',
+        fields: {
+          add: {
+            url: {
+              label: 'URL',
+              type: 'url',
+              required: true,
+            },
+            'social-link-icon': {
+              label: 'Icon',
+              type: 'area',
+              options: {
+                widgets: {
+                  icon: {
+                    background: 'black',
+                  },
+                },
+                max: 1,
+              },
+              required: true,
+            },
+          },
+        },
+      },
     },
     group: {
       links: {
         label: 'Links',
         fields: [
-          'headerNav', 'footerNav', 'footerLegal',
+          'headerNav', 'footerNav', 'footerLegal', 'social',
         ],
       },
     },
