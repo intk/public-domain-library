7169d6eca432fde5e923d7814d99ddc7217ef50a
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-07-04 17:36:57 +0200

    Authors page nav: highlight clicked letters

--- server/modules/author-page/ui/src/index.js
+++ server/modules/author-page/ui/src/index.js
@@ -1,6 +1,14 @@
 export default () => {
-  const author = document.querySelector('.pdl-authors__letter--A')
+  const author = document.querySelector('.pdl-authors__nav-letter--A')
   if (author) {
     author.classList.add('highlight')
   }
+
+  const authors = document.querySelectorAll('.pdl-authors__nav-letter')
+  authors.forEach(author => {
+    author.addEventListener('click', () => {
+      authors.forEach(a => a.classList.remove('highlight'))
+      author.classList.add('highlight')
+    })
+  })
 }
