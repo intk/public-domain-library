4013a0a2916049353051309994e96e4ce66709a0
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-07-05 14:32:49 +0200

    Authors mobile nav: highlight on scroll

--- server/modules/author-page/ui/src/index.js
+++ server/modules/author-page/ui/src/index.js
@@ -1,14 +1,50 @@
 export default () => {
-  const author = document.querySelector('.pdl-authors__nav-letter--A')
-  if (author) {
-    author.classList.add('highlight')
+  // highlight A by default
+  const letterA = document.querySelector('.pdl-authors__nav-letter--A')
+  if (letterA) {
+    letterA.classList.add('highlight')
   }
 
-  const authors = document.querySelectorAll('.pdl-authors__nav-letter')
-  authors.forEach(author => {
-    author.addEventListener('click', () => {
-      authors.forEach(a => a.classList.remove('highlight'))
-      author.classList.add('highlight')
-    })
-  })
+  observeVisibleLetter()
+
+  function observeVisibleLetter () {
+    const letters = document.querySelectorAll('.pdl-authors__letter')
+    const navLetters = document.querySelectorAll('.pdl-authors__nav-letter')
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1,
+    }
+
+    const callback = (entries, observer) => {
+      if (window.location.hash) {
+        const hash = window.location.hash.replace('#', '')
+        history.pushState('', document.title, window.location.pathname)
+        const visibleLetter = document.getElementById(hash)
+        navLetters.forEach(navLetter => {
+          navLetter.classList.remove('highlight')
+          if (navLetter.innerHTML === visibleLetter.id) {
+            navLetter.classList.add('highlight')
+          }
+        })
+
+        return
+      }
+
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const visibleLetter = entry.target
+          navLetters.forEach(navLetter => {
+            navLetter.classList.remove('highlight')
+            if (navLetter.innerHTML === visibleLetter.id) {
+              navLetter.classList.add('highlight')
+            }
+          })
+        }
+      })
+    }
+
+    const observer = new IntersectionObserver(callback, options)
+    letters.forEach(letter => observer.observe(letter))
+  }
 }
