6db08280d11b92dee3d912322eb1495cf845ed0c
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-07-23 17:04:42 +0200

    Author page books with tablet breakpoint

--- server/modules/book-page/ui/src/index.js
+++ server/modules/book-page/ui/src/index.js
@@ -1,4 +1,5 @@
 export default () => {
+  // display book formats when clicking on 3 dots (next to "Start reading" button)
   const formatsLists = document.querySelectorAll('.pdl-book-preview__formats')
   if (formatsLists?.length) {
     for (const formatsList of formatsLists) {
@@ -12,6 +13,19 @@ export default () => {
           thirdFormat.classList.toggle('pdl-book-preview__format--visible')
         })
       }
+    }
+  }
+
+  // display "View" button on mobile and tablet devices
+  const previews = document.querySelectorAll('[data-book-preview]')
+  if (previews?.length) {
+    for (const preview of previews) {
+      preview.addEventListener('click', () => {
+        const previewViewButton = preview.querySelector('[data-book-preview-view]')
+        if (previewViewButton) {
+          previewViewButton.classList.add('pdl-book-preview__view--visible')
+        }
+      })
     }
   }
 }
