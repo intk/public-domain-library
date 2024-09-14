aa56fc0661f0c9532fdb5fa1680b0c2e488b99db
Anthony Tarlao <anthony.tarlao@gmail.com>
2024-09-14 17:38:44 +0200

    Homepage covers

--- server/modules/@apostrophecms/home-page/index.js
+++ server/modules/@apostrophecms/home-page/index.js
@@ -4,39 +4,18 @@ module.exports = {
   },
   fields: {
     add: {
-      main: {
-        type: 'area',
-        options: {
-          widgets: {
-            '@apostrophecms/rich-text': {
-              toolbar: [
-                'styles',
-                '|',
-                'bold',
-                'italic',
-                'strike',
-                'link',
-                '|',
-                'bulletList',
-                'orderedList',
-              ],
-              styles: [
-                {
-                  tag: 'p',
-                  label: 'Paragraph (P)',
-                },
-                {
-                  tag: 'h3',
-                  label: 'Heading 3 (H3)',
-                },
-                {
-                  tag: 'h4',
-                  label: 'Heading 4 (H4)',
-                },
-              ],
-            },
-            '@apostrophecms/image': {},
-            '@apostrophecms/video': {},
+      backgroundText: {
+        type: 'string',
+      },
+      _coverBooks: {
+        type: 'relationship',
+        withType: 'book',
+        max: 9,
+        builders: {
+          project: {
+            title: 1,
+            _url: 1,
+            imageArea: 1,
           },
         },
       },
@@ -46,7 +25,8 @@ module.exports = {
         label: 'Basics',
         fields: [
           'title',
-          'main',
+          'backgroundText',
+          '_coverBooks',
         ],
       },
     },
