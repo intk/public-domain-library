34aa600ff208b94277a1b1e949c752b8efd789ae
Anthony Tarlao <anthony.tarlao@gmail.com>
2025-01-17 10:33:27 +0100

    Verify Google reCaptcha

--- server/modules/contact/index.js
+++ server/modules/contact/index.js
@@ -12,14 +12,28 @@ module.exports = {
       post: {
         async submit (req, res) {
           const locale = req.body.locale || 'en'
-          let contactUrl = 'contact'
+          const contactUrl = 'contact'
           const options = {
             from: config.get('nodemailer.auth.user'),
             to: config.get('nodemailer.auth.user'),
             subject: 'Public Domain Library - New contact submission',
           }
 
+          console.log('req.body[g-recaptcha-response] ====> ', req.body['g-recaptcha-response'])
+
           try {
+            const body = JSON.stringify({
+              secret: '6LdfWLoqAAAAAGIIJoyuu6_JCDycm7_kdiRtZ1-y',
+              response: req.body['g-recaptcha-response'],
+            })
+            const challenge = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body,
+            })
+            console.log('challenge ====> ', await challenge.json())
             await self.email(req, 'email.html', { body: req.body }, options)
             const referer = req.headers.referrer?.split('/') || []
             contactUrl = referer[referer.length - 1] || contactUrl
