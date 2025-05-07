46ca70dd41a25995a4eef3f0bc20a807ae119ff3
Anthony Tarlao <anthony.tarlao@gmail.com>
2025-05-07 18:02:19 +0200

    Test deploy

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -17,7 +17,6 @@ zero_downtime_deploy() {
   new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
   curl --silent --include --retry-connrefused --retry 30 --retry-delay 1 --fail http://$new_container_ip:3000/ || exit 1
   NODE_ENV=production docker compose -p library --compatibility build
-  docker exec $new_container_id sh -c "./scripts/release-task.sh"
 
   # start routing requests to the new container (as well as the old)
   reload_nginx
@@ -27,6 +26,7 @@ zero_downtime_deploy() {
   docker rm $old_container_id
 
   docker compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
+  docker exec $new_container_id sh -c "./scripts/release-task.sh"
 
   # stop routing requests to the old container
   reload_nginx
